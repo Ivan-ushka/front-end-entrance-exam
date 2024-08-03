7ef58203bfab737e69ec02159ff8cb186ea16557
Ivan Levchuk <ivanlev011287@gmail.com>
2024-08-03 21:17:48 +0300

    Add save changes and save pdf page logic

---
+++ src/js/saveChanges.js
@@ -0,0 +1,27 @@
+function loadData() {
+    const editableElements = document.querySelectorAll('[contenteditable="true"]');
+
+    editableElements.forEach((element) => {
+        const key = element.className ? element.classList[0] : element.tagName.toLowerCase();
+
+        const storedValue = localStorage.getItem(key);
+
+        element.textContent = storedValue !== null ? storedValue : element.textContent; // Если нет в localStorage, использовать из HTML
+    });
+}
+
+function saveData(event) {
+    const key = event.target.className ? event.target.classList[0] : event.target.tagName.toLowerCase();
+
+    localStorage.setItem(key, event.target.textContent);
+}
+
+window.onload = loadData;
+
+// Слушаем изменения только в редактируемых элементах
+
+const editableElements = document.querySelectorAll('[contenteditable="true"]');
+
+editableElements.forEach(element => {
+    element.addEventListener('input', saveData);
+});
---
+++ src/js/savePdf.js
@@ -0,0 +1,22 @@
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+    const firstSection = document.querySelector(".first-section");
+    const secondSection = document.querySelector(".second-section");
+    const thirdSection = document.querySelector(".third-section");
+
+    const opt = {
+        margin: 0.001,
+        filename: 'resume.pdf',
+        image: { type: 'jpeg', quality: 1.5 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    const pdfContent = document.createElement('div');
+    pdfContent.appendChild(firstSection.cloneNode(true));
+    pdfContent.appendChild(secondSection.cloneNode(true));
+    pdfContent.appendChild(thirdSection.cloneNode(true));
+
+
+    html2pdf().from(pdfContent).set(opt).save();
+});
